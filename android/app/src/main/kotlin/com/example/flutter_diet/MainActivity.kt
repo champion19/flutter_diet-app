0ec1ec97ba2c455494e4b981d53f349a7ea7457d
champion19e <emmalondogo@gmaill.com>
2024-01-16 19:15:22 -0500

    flutte_dietr app

---
+++ android/app/src/main/kotlin/com/example/flutter_diet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_diet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/Home.dart
@@ -0,0 +1,30 @@
+import "package:flutter/material.dart";
+
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xfff7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}

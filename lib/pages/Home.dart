dec1bdf3749c14d1abde73c9307e512d7dfd1108
champion19e <emmalondogo@gmaill.com>
2024-01-20 10:35:59 -0500

    lista categoria

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -29,59 +29,78 @@ class _HomePageState extends State<Homepage> {
           const SizedBox(
             height: 40,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Category',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                height: 150,
-                child: ListView.separated(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  separatorBuilder: (context, index) => const SizedBox(
-                    width: 25,
-                  ),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          color: categories[index].boxColor.withOpacity(0.3),
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                                color: Colors.white, shape: BoxShape.circle),
-                            child: SvgPicture.asset(categories[index].iconPath),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              )
-            ],
-          )
+          _categoriesSection()
         ],
       ),
-    );
+       );
+        }
+
+  Column _categoriesSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                'Category',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 150,
+              child: ListView.separated(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 25,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                        color: categories[index].boxColor.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: const BoxDecoration(
+                              color: Colors.white, shape: BoxShape.circle
+                              ),
+                              child:Padding (
+                                padding:const EdgeInsets.all(8.0),
+                                 child: SvgPicture.asset(categories[index].iconPath),
+                        ),
+                        ),
+                        Text(
+                          categories[index].name,
+                          style:const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color:Colors.black,
+                            fontSize: 14
+                          ),
+                        ),
+
+
+
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        );
   }
 
   Container _searchField() {

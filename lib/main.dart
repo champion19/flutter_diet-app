d2ccdb47aa484eed7130881364dbc04304a1438e
champion19e <emmalondogo@gmaill.com>
2024-01-21 11:07:28 -0500

    popularDiet listo

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Poppins'),
-        home:  Homepage()
-        );
+        home: HomePage());
   }
 }

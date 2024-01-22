bde17d97b5b9e3b4437b3f389071e733d16144ed
champion19e <emmalondogo@gmaill.com>
2024-01-22 08:48:56 -0500

    actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_diet/pages/Home.dart';
-import 'package:intl/date_symbol_data_http_request.dart';
-import 'package:intl/intl.dart';
-import 'package:intl/number_symbols_data.dart';
+
 
 void main() {
-  Intl.defaultLocale = 'en_US';
-  //initializeDateFormatting('es_ES');
-
   runApp(const MyApp());
 }
 

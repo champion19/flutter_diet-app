d24e5315b6b5ac82e2422d19c2cfc8cc3cc09dc4
champion19e <emmalondogo@gmaill.com>
2024-01-24 12:04:42 -0500

    defaultLocale,languageCode

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_diet/pages/Home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'dart:ui' as ui;
+
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
+
+  final languageCode = ui.window.locale.languageCode;
+  final countryCode = ui.window.locale.countryCode;
+  String defaultlocale = languageCode;
+  if (countryCode != null) {
+    defaultlocale += '_$countryCode';
+  }
+  print(defaultlocale);
+  Intl.defaultLocale = defaultlocale;
 }
 
 class MyApp extends StatelessWidget {
@@ -14,19 +26,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Poppins'),
-        home: HomePage(),
-localizationsDelegates:const [
-  AppLocalizations.delegate,
-  GlobalMaterialLocalizations.delegate,
-  GlobalCupertinoLocalizations.delegate,
-  GlobalWidgetsLocalizations.delegate
-],
-supportedLocales:const [
-  Locale('en'),
-  Locale('es'),
-] ,
-);
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: HomePage(),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('es'),
+      ],
+    );
   }
 }

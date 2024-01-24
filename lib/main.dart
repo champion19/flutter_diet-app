ceb12c76c8174187fcc0efa77cec1ed757994b3b
champion19e <emmalondogo@gmaill.com>
2024-01-24 10:56:49 -0500

    agrego flutter_gen y globalLocalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_diet/pages/Home.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Poppins'),
-        home: HomePage());
+        home: HomePage(),
+localizationsDelegates:const [
+  AppLocalizations.delegate,
+  GlobalMaterialLocalizations.delegate,
+  GlobalCupertinoLocalizations.delegate,
+  GlobalWidgetsLocalizations.delegate
+],
+supportedLocales:const [
+  Locale('en'),
+  Locale('es'),
+] ,
+);
   }
 }
